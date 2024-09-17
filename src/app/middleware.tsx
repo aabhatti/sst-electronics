a40d6e273febfd18912320d9d8bf5fc3b009dc41
ADNAN AHMAD <adnan.ahmad@kinectro.com.au>
2024-09-17 15:43:28 +0500

    disable auth files

--- src/app/middleware.tsx
+++ src/app/middleware.tsx
@@ -1,31 +1,31 @@
-import {
-  apiAuthPrefix,
-  publicRoutes,
-  auhRoutes,
-  adminRoutes,
-  DEFAULT_LOGGED_IN_REDIRECT,
-} from "@/utils/routes";
-import { auth } from "./auth";
+// import {
+//   apiAuthPrefix,
+//   publicRoutes,
+//   auhRoutes,
+//   adminRoutes,
+//   DEFAULT_LOGGED_IN_REDIRECT,
+// } from "@/utils/routes";
+// import { auth } from "./auth";
 
-export default auth((req) => {
-  const { nextUrl } = req;
-  const isLoggedIn = !!req.auth;
-  console.log("req.auth>>", req.auth);
-  const isApiAuthRoutes = nextUrl.pathname.startsWith(apiAuthPrefix);
-  const isPublicRoute = publicRoutes.includes(nextUrl.pathname);
-  const isAuthRoute = auhRoutes.includes(nextUrl.pathname);
-  if (isApiAuthRoutes) return;
+// export default auth((req) => {
+//   const { nextUrl } = req;
+//   const isLoggedIn = !!req.auth;
+//   console.log("req.auth>>", req.auth);
+//   const isApiAuthRoutes = nextUrl.pathname.startsWith(apiAuthPrefix);
+//   const isPublicRoute = publicRoutes.includes(nextUrl.pathname);
+//   const isAuthRoute = auhRoutes.includes(nextUrl.pathname);
+//   if (isApiAuthRoutes) return;
 
-  if (isLoggedIn && isAuthRoute) {
-    return Response.redirect(
-      new URL(DEFAULT_LOGGED_IN_REDIRECT, nextUrl.pathname)
-    );
-  }
-  if (!isLoggedIn && !isPublicRoute) {
-    return Response.redirect(new URL(auhRoutes[0], nextUrl.pathname));
-  }
-  return;
-});
+//   if (isLoggedIn && isAuthRoute) {
+//     return Response.redirect(
+//       new URL(DEFAULT_LOGGED_IN_REDIRECT, nextUrl.pathname)
+//     );
+//   }
+//   if (!isLoggedIn && !isPublicRoute) {
+//     return Response.redirect(new URL(auhRoutes[0], nextUrl.pathname));
+//   }
+//   return;
+// });
 
 // Optionally, don't invoke Middleware on some paths
 export const config = {
