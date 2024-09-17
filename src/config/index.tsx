c4474ae79d1bcbd9cab84b52ed441f0f2eae2602
ADNAN AHMAD <adnan.ahmad@kinectro.com.au>
2024-09-17 16:30:18 +0500

    update the config url

--- src/config/index.tsx
+++ src/config/index.tsx
@@ -1,4 +1,7 @@
-const serverUrl =
-  process.env.NEXT_PUBLIC_APP_URL || "http://localhost:3007/api";
+const serverUrl = process.env.NEXT_PUBLIC_APP_URL
+  ? process.env.NEXT_PUBLIC_APP_URL
+  : process.env.MODE === "production"
+  ? "https://sst-electronics-git-master-aabhattis-projects.vercel.app/api"
+  : "http://localhost:3007/api";
 
 export { serverUrl };
