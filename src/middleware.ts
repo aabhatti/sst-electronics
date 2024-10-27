9692cb966f3344e4bc11809ea8aae9dc1e351c4c
ADNAN AHMAD <adnan.ahmad@kinectro.com.au>
2024-10-27 21:44:18 +0500

    update middleware config file

--- src/middleware.ts
+++ src/middleware.ts
@@ -45,4 +45,5 @@ export { auth as middleware } from "./auth";
 
 export const config = {
   matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)", "/"],
+  unstable_allowDynamic: ["../node_modules/mongoose/dist/browser.umd.js"],
 };
